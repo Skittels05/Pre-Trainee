27dec7f05471712f5ec6b009cd1a9c246a4214da
Skittels05 <skittels261105@gmail.com>
2025-10-06 21:24:25 +0300

    feat: add ex2

---
+++ Week1/Ex2/Ex2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var files = new List<string>
+            {
+                "Файл1",
+                "Файл2",
+                "Файл3"
+            };
+            var syncStopwatch = Stopwatch.StartNew();
+            foreach (var file in files)
+            {
+                var result = ProcessData(file);
+                Console.WriteLine($"Обработка {result} завершена за 3 секунды");
+            }
+            syncStopwatch.Stop();
+            Console.WriteLine($"Синхронная обработка завершена за {syncStopwatch.Elapsed.TotalSeconds:F2} секунд");
+
+            var asyncStopwatch = Stopwatch.StartNew();
+            var asyncFiles = new List<Task<string>>
+            {
+                ProcessDataAsync("Файл1"),
+                ProcessDataAsync("Файл2"),
+                ProcessDataAsync("Файл3")
+            };
+            while (asyncFiles.Count > 0)
+            {
+                var completedTask = await Task.WhenAny(asyncFiles);
+                string result = await completedTask;
+                Console.WriteLine($"Обработка {result} завершена за 3 секунды");
+                asyncFiles.Remove(completedTask); 
+            }
+            asyncStopwatch.Stop();
+            Console.WriteLine($"Асинхронная обработка завершена за {asyncStopwatch.Elapsed.TotalSeconds:F2} секунд");
+        }
+        static string ProcessData(string dataName)
+        {
+            Thread.Sleep(3000);
+            return dataName;
+        }
+        static async Task<string> ProcessDataAsync(string dataName)
+        {
+            await Task.Delay(3000);
+            return dataName;
+        }
+
+    }
+}
